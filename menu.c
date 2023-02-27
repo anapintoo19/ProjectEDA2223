c87f20fcddabafe64bc512de5db2e23d4aacd7aa
anapintoo19 <anammpinto.fcp@gmail.com>
2023-02-27 22:59:03 +0000

    mostrarCliente

--- menu.c
+++ menu.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <locale.h>
 
+#pragma region MENUS
 
 void mostrarMenu() {
+	printf("***********************************\n");
+	printf("*             MENU                *\n");
+	printf("***********************************\n");
+	printf("* 1. Cliente                      *\n");
+	printf("* 2. Gestor                       *\n");
+	printf("* 3. Mobilidade                   *\n");
+	printf("* 0. Sair                         *\n");
+	printf("***********************************\n");
+	printf("Opção: ");
+}
+
+void mostrarCliente() {
 
 }

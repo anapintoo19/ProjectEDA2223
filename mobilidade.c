0fc313057fa129592be65b52f12517b275d631ab
anapintoo19 <anammpinto.fcp@gmail.com>
2023-03-16 23:18:56 +0000

    removerMobilidade

--- mobilidade.c
+++ mobilidade.c
@@ -11,14 +11,42 @@ void listarMobilidade(Mobilidade* mobilidade) {
 
 	while (mobilidade != NULL)
 	{
-		printf("ID: %d    Tipo de Mobilidade: %s    Nível de Bateria: %.2f   Autonomia:%.2f  Mobilidade Alugada? %d", mobilidade->idMobilidade, mobilidade->tipo, mobilidade->nivel_bateria, mobilidade->autonomia, mobilidade->mobilidadeAlugada);
+		printf("ID: %d    Tipo de Mobilidade: %s    Nível de Bateria: %.2f   Autonomia:%.2f  Mobilidade Alugada? %d", mobilidade->idMobilidade, mobilidade->tipo, mobilidade->nivel_bateria, mobilidade->autonomia);
+		mobilidade = mobilidade->seguinte;
 	}
 	printf("\n****************************************************************************\n");
 }
 
 // Função para criar um novo registo de uma nova Mobilidade
 
-Mobilidade* inserirMobilidade(Mobilidade* mobilidade, int idMobilidade, char tipo[], float nivel_bateria, float autonomia, int mobilidadeAlugada) {
+Mobilidade* inserirMobilidade(Mobilidade* mobilidade, int idMobilidade, char tipo[], float nivel_bateria, float autonomia) {
+
+	if (!existeMobilidade(mobilidade, idMobilidade))
+	{
+		Mobilidade* novo = malloc(sizeof(struct mobilidade));
+
+		if (novo != NULL)
+		{
+			novo->idMobilidade = idMobilidade;
+			strcpy(novo->tipo, tipo);
+			novo->nivel_bateria = nivel_bateria;
+			novo->autonomia = autonomia;
+			novo->seguinte = mobilidade;
+			return(novo);
+		}
+		else
+		{
+			return(mobilidade);
+		}
+	}
+
+	
+}
+
+// Função para verificar se um registo de uma mobilidade já existe pelo seu ID
+
+int existeMobilidade(Mobilidade* mobilidade, int idMobilidade) {
+
 	Mobilidade* novo = (Mobilidade*)malloc(sizeof(Mobilidade));
 
 	if (novo != NULL)
@@ -27,12 +55,49 @@ Mobilidade* inserirMobilidade(Mobilidade* mobilidade, int idMobilidade, char tip
 		strcpy(novo->tipo, tipo);
 		novo->nivel_bateria = nivel_bateria;
 		novo->autonomia = autonomia;
-		novo->mobilidadeAlugada = mobilidadeAlugada;
 		novo->seguinte = mobilidade;
 		return(novo);
 	}
 	else
 	{
 		return(mobilidade);
+	}
+}
+
+// Função para remover um registo de uma mobilidade pelo ID
+
+Mobilidade* removerMobilidade(Mobilidade* mobilidade, int idMobilidade) {
+
+	Mobilidade* anterior = mobilidade, * atual = mobilidade, * aux;
+
+	if (atual == NULL)
+	{
+		return NULL;
+	}
+	else
+	{
+		if (atual->idMobilidade == idMobilidade)
+		{
+			aux = atual->seguinte;
+			free(atual);
+		}
+		else
+		{
+			while ((atual != NULL) && (atual->idMobilidade != idMobilidade))
+			{
+				anterior = atual;
+				atual = atual->seguinte;
+			}
+			if (atual == NULL)
+			{
+				return(mobilidade);
+			}
+			else
+			{
+				anterior->seguinte;
+				free(atual);
+				return(mobilidade);
+			}
+		}
 	}
 }

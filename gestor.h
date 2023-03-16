6c5252a63da92f2f9d3bd410744faf87cf157899
anapintoo19 <anammpinto.fcp@gmail.com>
2023-03-16 23:41:39 +0000

    declaraçãodasFunções

--- gestor.h
+++ gestor.h
@@ -23,4 +23,7 @@ typedef struct gestor
 //Declaração das funções para os gestores
 
 void listarGestores(Gestor* gestor);
+int existeGestor(Gestor* gestor, int idGestor);
 Gestor* inserirGestor(Gestor* gestor, int idGestor, char nomeGestor[], char password[]);
+Gestor* removerGestor(Gestor* gestor, int idGestor);
+Gestor* alterarGestor(Gestor* gestor, int idGestor, char nomeGestorNovo[], char passwordNova[]);
